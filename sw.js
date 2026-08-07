83739f185fcc91ff292ac524f7d878dcbc9aba27
rinmefornoodles-max <rinmefornoodles@gmail.com>
2026-08-07 01:55:44 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-const CACHE='commander-forge-5.28.0'; self.addEventListener('install',()=>self.skipWaiting()); self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+const CACHE='commander-forge-5.29.0'; self.addEventListener('install',()=>self.skipWaiting()); self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
