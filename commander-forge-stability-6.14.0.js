96a579a9b759b5d29d3a5854be48548d2931f69f
rinmefornoodles-max <rinmefornoodles@gmail.com>
2026-08-20 00:09:54 -0700

    Add Commander Forge 6.14 stability layer

---
+++ commander-forge-stability-6.14.0.js
@@ -0,0 +1,7 @@
+(() => {
+  'use strict';
+  const PRODUCT_VERSION = '6.14.0';
+  const BUNDLE_VERSION = '6.13.0';
+  window.CommanderForgeProductVersion = PRODUCT_VERSION;
+  window.CommanderForgeExpectedBundleVersion = BUNDLE_VERSION;
+})();
